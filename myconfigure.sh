fc8086fc0efd476760826c71968ab270cadd566e
Ben Martin <monkeyiq@users.sourceforge.net>
2015-01-28 14:21:07 +1000

    .

---
+++ myconfigure.sh
@@ -0,0 +1,10 @@
+echo "This is the monkeyiq config invoker. please don't use it."
+#export CFLAGS=" -fPIC "
+#export CXXFLAGS=" -fPIC "
+/Develop/libstldb4/configure \
+--enable-wrapdebug --enable-rpc --with-uniquename=stldb4 --enable-hiddensymbols \
+--disable-stlport --with-sigcxx-2x=yes \
+--prefix=/usr/local --libdir=/usr/local/lib64
+
+
+#--prefix=/usr --libdir=/usr/lib 
